f25567a4e97e3628a2d42a7f7ef5ed3f30d82db9
sonic <sonicgd@gmail.com>
2014-05-03 22:14:52 +0600

    fill db settings

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=selfpub',
+    'username' => 'selfpub',
+    'password' => 'hmEcM3iuGu6ZBVc',
     'charset' => 'utf8',
 ];

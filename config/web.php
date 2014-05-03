47dba247cc3468f24f8a143bcecfeb436e60965d
sonic <sonicgd@gmail.com>
2014-05-03 22:46:10 +0600

    Enable redis cache

--- config/web.php
+++ config/web.php
@@ -4,38 +4,45 @@ $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
 
 $config = [
-    'id' => 'basic',
-    'name' => 'Selfpub',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'id'         => 'basic',
+    'name'       => 'Selfpub',
+    'basePath'   => dirname(__DIR__),
+    'bootstrap'  => ['log'],
     'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
     'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
+        'cache'        => [
+            'class'     => \yii\redis\Cache::className(),
+            'keyPrefix' => 'selfpub'
         ],
-        'user' => [
-            'identityClass' => 'app\models\User',
+        'redis'        => [
+            'class'    => \yii\redis\Connection::className(),
+            'hostname' => 'localhost',
+            'port'     => 6379,
+            'database' => 5,
+        ],
+        'user'         => [
+            'identityClass'   => 'app\models\User',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mail' => [
-            'class' => 'yii\swiftmailer\Mailer',
+        'mail'         => [
+            'class'            => 'yii\swiftmailer\Mailer',
             'useFileTransport' => true,
         ],
-        'log' => [
+        'log'          => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
+            'targets'    => [
                 [
-                    'class' => 'yii\log\FileTarget',
+                    'class'  => \yii\log\FileTarget::className(),
                     'levels' => ['error', 'warning'],
                 ],
             ],
         ],
-        'db' => $db,
+        'db'           => $db,
     ],
-    'params' => $params,
+    'params'     => $params,
 ];
 
 if (YII_ENV_DEV) {

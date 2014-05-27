6b58ac250b43e4765cbbd0c06c15644bc1881704
SonicGD <sonicgd@gmail.com>
2014-05-27 21:54:48 +0600

    begin form

--- views/requests/add.php
+++ views/requests/add.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * @var \app\models\Request $request
+ */
+use yii\bootstrap\ActiveForm;
 
 $this->title = "Добавить запрос";
-
+$form = ActiveForm::begin(['layout' => 'horizontal']);
 ?>
-
-123
+<?= $form->field($request, 'bookName')->textInput() ?>
+<?php
+$form->end();

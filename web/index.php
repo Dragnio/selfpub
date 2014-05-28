b11f75295dccc3e229faf3b94c5c55caa090af23
SonicGD <sonicgd@gmail.com>
2014-05-29 00:17:36 +0600

    debug

--- web/index.php
+++ web/index.php
@@ -2,12 +2,13 @@
 
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENV') or define('YII_ENV', 'test');
 defined('ROOT_DIR') or define('ROOT_DIR', dirname(__FILE__));
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
-
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
 (new yii\web\Application($config))->run();

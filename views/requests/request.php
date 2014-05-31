be6c00e64d020670ac689a319eb4a8492a9a73b9
SonicGD <sonicgd@gmail.com>
2014-05-31 17:26:47 +0600

    show lang

--- views/requests/request.php
+++ views/requests/request.php
@@ -6,7 +6,10 @@ use app\models\Request;
  */
 
 $this->title = $request->bookName;
-
+$langs = [
+    'Русский',
+    'Английский'
+];
 ?>
 <div class="row">
     <div class="col-lg-2"><img src="<?= $request->getCoverUrl() ?>" alt="<?= $request->bookName ?>"/></div>
@@ -14,5 +17,7 @@ $this->title = $request->bookName;
         <p>Автор: <b><?= $request->authorName ?></b></p>
 
         <p><?= $request->synopsis ?></p>
+
+        <p>Язык: <b><?= $langs[$request->language] ?></b></p>
     </div>
 </div>
